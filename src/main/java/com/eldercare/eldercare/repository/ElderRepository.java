250183d204d9f6e565382630fa76c32d0b9ca20f
Hirushan <kavinduhirushan78@gmail.com>
2026-02-14 16:50:30 +0530

    Remove mongo

--- src/main/java/com/eldercare/eldercare/repository/ElderRepository.java
+++ src/main/java/com/eldercare/eldercare/repository/ElderRepository.java
@@ -1,7 +1,6 @@
 package com.eldercare.eldercare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.eldercare.eldercare.model.Elder;
 import java.util.Optional;
--- src/main/java/com/eldercare/eldercare/repository/HealthRecordRepository.java
+++ src/main/java/com/eldercare/eldercare/repository/HealthRecordRepository.java
@@ -1,7 +1,7 @@
 package com.eldercare.eldercare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
+
 import org.springframework.stereotype.Repository;
 import com.eldercare.eldercare.model.DailyHealthRecord;
 import java.util.Optional;
